c4c1666babc9cbd775625b08fd35f234d60c2fc8
김승민_T6023 <78553350+viitamin@users.noreply.github.com>
2024-01-26 10:54:26 +0900

    Feat: baseline update (#9)
    * Develop (#3)
    
    * Feat: baseline code initialize
    
    * Docs: github template initialize
    
    * Remove: 필요 없는 파일 삭제
    
    * Update: modified train.py, utils.py
    1. sh 파일에서 wandb_name만 지정해주면 checkpoint가 저장될 폴더 이름, wandb run name 이 한번에 지정되도록 변경
    2. train.py의 checkpoint 경로 예외 처리 코드를 간소화
    
    * Feat: config.json 파일 생성 코드 추가
    
    * Update: modified train.py, inference.py
    1. inference.py에서 submission 경로 생성 코드 간소화
    2. train.py에서 pbar를 사용하여 training log를 출력하는 대신 print문으로 직접 구현
    3. training log를 출력할 때 총 남은 시간 계산하는 코드 추가
    
    * Update: modified train.py, inference.py
    train.py - delete 'non_blocking=True'
    inference.py - change 'submission_{checkpoint}.csh' to 'output.csv'
    
    * Remove: delete trainer.py
    
    * Feat: baesline code update
    train.py - validation loop 적용
    inference.py - output.csv 가 저장되는 폴더 이름을 submissions -> predictions 로 변경
    dataset.py - 이미지 경로를 split 이라는 변수로 받던 것을 train 폴더로 직접 명시
    group_k_fold.ipynb - validation dataset을 분할해주는 코드
    visualization.ipynb - 학습한 모델의 추론 결과를 시각화해주는 코드
    
    issue: #6
    
    ---------
    
    Co-authored-by: 조성혁_T6164 <whtjdgur77@gmail.com>

--- code/train.py
+++ code/train.py
@@ -28,12 +28,15 @@ def parse_args():
     # Conventional args
     parser.add_argument("--seed", type=int, default=4669)
     parser.add_argument("--data_dir", type=str, default="../data/medical")
-    parser.add_argument("--model_dir", type=str, default="checkpoints")
+    parser.add_argument("--train_ann", type=str, default="train1")
+    parser.add_argument("--valid_ann", type=str, default="valid1")
+    parser.add_argument("--model_dir", type=str, default="trained_models")
     parser.add_argument("--device", default="cuda" if cuda.is_available() else "cpu")
     parser.add_argument("--num_workers", type=int, default=8)
     parser.add_argument("--image_size", type=int, default=2048)
     parser.add_argument("--input_size", type=int, default=1024)
     parser.add_argument("--batch_size", type=int, default=8)
+    parser.add_argument("--patience", type=int, default=10)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
     parser.add_argument("--max_epochs", type=int, default=150)
     parser.add_argument("--save_interval", type=int, default=5)
@@ -59,8 +62,8 @@ def seed_everything(seed):
     os.environ["PYTHONHASHSEED"] = str(seed)
 
 
-def do_training(config, seed, data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
-                learning_rate, max_epochs, save_interval, ignore_tags, wandb_name):
+def do_training(config, seed, data_dir, train_ann, valid_ann, model_dir, device, image_size, input_size, num_workers, batch_size,
+                patience, learning_rate, max_epochs, save_interval, ignore_tags, wandb_name):
     seed_everything(seed)  # set seed
     
     # checkpoint directory initialization
@@ -76,47 +79,74 @@ def do_training(config, seed, data_dir, model_dir, device, image_size, input_siz
                name=model_name,
                config=config)
     
-    dataset = SceneTextDataset(
+    train_dataset = SceneTextDataset(
         data_dir,
-        split="train",
+        split=train_ann,
         image_size=image_size,
         crop_size=input_size,
         ignore_tags=ignore_tags
     )
-    dataset = EASTDataset(dataset)
-    num_batches = math.ceil(len(dataset) / batch_size)
+    train_dataset = EASTDataset(train_dataset)
+    
     train_loader = DataLoader(
-        dataset,
+        train_dataset,
         batch_size=batch_size,
         shuffle=True,
         num_workers=num_workers
     )
+    
+    valid_dataset = SceneTextDataset(
+        data_dir,
+        split=valid_ann,
+        image_size=image_size,
+        crop_size=input_size,
+        ignore_tags=ignore_tags
+    )
+    valid_dataset = EASTDataset(valid_dataset)
+    
+    valid_loader = DataLoader(
+        valid_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers
+    )
+    
+    train_num_batches = math.ceil(len(train_dataset) / batch_size)
+    valid_num_batches = math.ceil(len(valid_dataset) / batch_size)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = EAST()
     model.to(device)
+    
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epochs // 2], gamma=0.1)
     
-    model.train()
+    # early stopping
+    counter = 0
+    best_val_loss = np.inf
+    
+    # ========== train ==========
     for epoch in range(max_epochs):
-        epoch_loss, epoch_start = 0, time.time()
+        model.train()
+        train_loss, valid_loss, train_start = 0, 0, time.time()
+        
         for idx, (img, gt_score_map, gt_geo_map, roi_mask) in enumerate(train_loader):
-            
             loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             loss_val = loss.item()
-            epoch_loss += loss_val
+            train_loss += loss_val
 
+            # logging at CLI
             print(
-                f"Epoch[{epoch+1}/{max_epochs}]({idx+1}/{len(train_loader)}) || "
-                f"Loss : {loss_val:4.4f} || "
-                f"Class loss: {extra_info['cls_loss']:4.4f} || "
-                f"Angle loss: {extra_info['angle_loss']:4.4f} || "
-                f"IoU loss: {extra_info['iou_loss']:4.4f}"
+                f"Epoch[{epoch+1:02}/{max_epochs}]({idx+1:02}/{len(train_loader)}) || "
+                f"Learning Rate: {scheduler.get_last_lr()[0]} || "
+                f"Train Loss: {loss_val:4.4f} || "
+                f"Train Class loss: {extra_info['cls_loss']:4.4f} || "
+                f"Train Angle loss: {extra_info['angle_loss']:4.4f} || "
+                f"Train IoU loss: {extra_info['iou_loss']:4.4f}"
             )
             
             # logging with wandb
@@ -125,20 +155,76 @@ def do_training(config, seed, data_dir, model_dir, device, image_size, input_siz
                 "Angle loss": extra_info['angle_loss'],
                 "IoU loss": extra_info['iou_loss'],
                 "loss": loss_val,
-                "Epochs": epoch+1
+                "Epochs": epoch+1,
+                "Learning Rate": scheduler.get_last_lr()[0],
+                "Seed": torch.seed()
             })
+        
+        # ========== valid ==========
+        model.eval()
+        with torch.no_grad():
+            valid_start = time.time()
+            print("\nEvaluating validation results...")
+            
+            for idx, (img, gt_score_map, gt_geo_map, roi_mask) in enumerate(valid_loader):
+                loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+                loss_val = loss.item()
+                valid_loss += loss_val
+                
+                # logging at CLI
+                print(
+                    f"Valid Batch({idx+1}/{len(valid_loader)}) || "
+                    f"Valid loss: {loss_val:4.4f} || "
+                    f"Valid Class loss: {extra_info['cls_loss']:4.4f} || "
+                    f"Valid Angle loss: {extra_info['angle_loss']:4.4f} || "
+                    f"Valid IoU loss: {extra_info['iou_loss']:4.4f}"
+                )
+                
+                # logging with wandb
+                wandb.log({
+                    "Valid loss": loss_val,
+                    "Valid Cls loss": extra_info['cls_loss'],
+                    "Valid Angle loss": extra_info['angle_loss'],
+                    "Valid IoU loss": extra_info['iou_loss'],
+                    })
 
+            # save only when best loss
+            mean_val_loss = valid_loss / valid_num_batches
+            if best_val_loss > mean_val_loss:
+                best_val_loss = mean_val_loss
+                best_val_loss_epoch = epoch+1
+                ckpt_fpath = osp.join(model_dir, f"best_epoch_{best_val_loss_epoch}.pth")
+                torch.save(model.state_dict(), ckpt_fpath)
+                counter = 0
+            else:
+                counter += 1
+                print(f"Not Val Update.. Counter : {counter}")
+
+        train_end = time.time() - train_start
+        print("Train Mean loss: {:.4f} || Elapsed time: {} || ETA: {}".format(
+            train_loss / train_num_batches,
+            timedelta(seconds=train_end),
+            timedelta(seconds=train_end*(max_epochs-epoch+1))))
+        
+        valid_end = time.time() - valid_start
+        print("Valid Mean loss: {:.4f} || Elapsed time: {}".format(
+            mean_val_loss,
+            timedelta(seconds=valid_end)))
+        
+        print("Best Validation Loss: {:.4f} at Epoch {}".format(
+            best_val_loss,
+            best_val_loss_epoch))
+        
+        # early stopping
+        if counter > patience:
+            print("Early Stopping!")
+            break
+        
         scheduler.step()
 
-        epoch_end = time.time() - epoch_start
-        print("Mean loss: {:.4f} || Elapsed time: {} || ETA: {}".format(
-            epoch_loss / num_batches,
-            timedelta(seconds=epoch_end),
-            timedelta(seconds=epoch_end*(max_epochs-epoch+1))))
-
-        if (epoch + 1) % save_interval == 0:
-            ckpt_fpath = osp.join(model_dir, "latest.pth")
-            torch.save(model.state_dict(), ckpt_fpath)
+        # if (epoch + 1) >= 50 and (epoch + 1) % save_interval == 0:
+        #     ckpt_fpath = osp.join(model_dir, f"epoch_{epoch+1}.pth")
+        #     torch.save(model.state_dict(), ckpt_fpath)
 
 
 def main(args):
